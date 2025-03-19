0906b6eddd1df4725a3b3b7df0262fc55a8f187f
22269 <2226975012@qq.com>
2025-03-19 09:38:20 +0800

    画布旋转拖动变换

--- src/MCCS/ViewModels/Pages/HomeTestOperationPageViewModel.cs
+++ src/MCCS/ViewModels/Pages/HomeTestOperationPageViewModel.cs
@@ -1,13 +1,50 @@
 ﻿
+using System.Windows.Input;
+
 namespace MCCS.ViewModels.Pages
 {
     public class HomeTestOperationPageViewModel : BaseViewModel
     {
         public const string Tag = "HomeTestOperationPage";
 
+        private bool _action = false;
+
         public HomeTestOperationPageViewModel(IEventAggregator eventAggregator, IDialogService dialogService) 
             : base(eventAggregator, dialogService)
         {
         }
+
+        #region 命令
+        public DelegateCommand<object> MouseRightButtonDownCommand => new(ExcuateMouseRightButtonDownCommand);
+        public DelegateCommand<object> MouseMoveCommand => new(ExcuateMouseMoveCommand);
+        public DelegateCommand<object> MouseRightButtonUpCommand => new(ExcuateMouseRightButtonUpCommand);
+        public DelegateCommand<object> MouseWheelCommand => new(ExcuateMouseWheelCommand);
+        #endregion
+
+        #region 私有方法
+        private void ExcuateMouseRightButtonDownCommand(object param) 
+        {
+
+        }
+
+        private void ExcuateMouseMoveCommand(object param) 
+        {
+
+        }
+
+        private void ExcuateMouseRightButtonUpCommand(object param) 
+        {
+
+        }
+
+        private void ExcuateMouseWheelCommand(object param) 
+        {
+            // 监听Crtl + 滚轮事件
+            if (Keyboard.IsKeyDown(Key.LeftCtrl) || Keyboard.IsKeyDown(Key.RightCtrl)) 
+            {
+
+            }
+        }
+        #endregion
     }
 }
