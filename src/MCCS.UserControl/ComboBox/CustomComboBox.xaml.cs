6f04c9ae9f9ff628dcc5985065c6369a1df706f3
kaizheng <2226975012@qq.com>
2025-05-14 14:09:52 +0800

    ComboBox自定义

---
+++ src/MCCS.UserControl/ComboBox/CustomComboBox.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+
+namespace MCCS.UserControl.ComboBox
+{
+    /// <summary>
+    /// CustomComboBox.xaml 的交互逻辑
+    /// </summary>
+    public partial class CustomComboBox : System.Windows.Controls.UserControl
+    {
+        public CustomComboBox()
+        {
+            InitializeComponent();
+        }
+
+        #region Dependency Properties
+        //public static readonly DependencyProperty ItemsSourceProperty =
+        //    DependencyProperty.Register(nameof(ItemsSource),
+        //        typeof(IEnumerable<object>),
+        //        typeof(CustomComboBox),
+        //        new PropertyMetadata(null));
+        #endregion
+    }
+}
