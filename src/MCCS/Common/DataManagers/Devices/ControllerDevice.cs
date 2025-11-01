a89b3118ef6e0681a819b167f3ddfb4563338482
kaizheng <2226975012@qq.com>
2025-11-01 17:48:27 +0800

    创建一个分支

--- src/MCCS/Common/DataManagers/Devices/ControllerDevice.cs
+++ src/MCCS/Common/DataManagers/Devices/ControllerDevice.cs
@@ -5,6 +5,7 @@ namespace MCCS.Common.DataManagers.Devices
 {
     public class ControllerDevice(long id, string name, long? parentDevicelong) : BaseDevice(id, name, DeviceTypeEnum.Controller, parentDevicelong)
     {
+
         public List<StationSiteControllerSignalInfo> SignalInfos { get; } = [];
     }
 }
