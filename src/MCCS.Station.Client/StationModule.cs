47dec1fd2dbe449588571a805bb919b614a125d1
kaizheng <2226975012@qq.com>
2026-01-04 21:01:10 +0800

    添加了一个新项目准备转移

---
+++ src/MCCS.Station.Client/StationModule.cs
@@ -0,0 +1,12 @@
+﻿namespace MCCS.Station.Client
+{
+    public sealed class StationModule : IModule
+    {
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+
+        public void OnInitialized(IContainerProvider containerProvider) => throw new NotImplementedException();
+    }
+}
